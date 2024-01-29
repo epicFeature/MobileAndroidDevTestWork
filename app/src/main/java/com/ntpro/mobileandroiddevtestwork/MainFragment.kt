d54aa894922567357ce13444df5b66b385136efe
mariasokolova0195@gmail.com <mariasokolova0195@gmail.com>
2024-01-29 18:43:16 +0300

    ViewModel created

--- app/src/main/java/com/ntpro/mobileandroiddevtestwork/MainFragment.kt
+++ app/src/main/java/com/ntpro/mobileandroiddevtestwork/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.ntpro.mobileandroiddevtestwork
 
+import android.icu.util.LocaleData
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,8 +10,11 @@ import androidx.navigation.fragment.findNavController
 import com.ntpro.mobileandroiddevtestwork.databinding.FragmentMainBinding
 import de.codecrafters.tableview.TableDataAdapter
 import de.codecrafters.tableview.toolkit.SimpleTableDataAdapter
+import java.time.LocalDate
 
 class MainFragment : Fragment() {
+    private lateinit var viewModel: MainViewModel
+
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
@@ -27,33 +31,8 @@ class MainFragment : Fragment() {
         val server = Server()
         val data = mutableListOf<Server.Deal>()
         server.subscribeToDeals { data }
-        val dealTableDataAdapter = DealTableDataAdapter(this.requireContext(), data)
+       // val dealTableDataAdapter = DealTableDataAdapter(this.requireContext(), )
     }
-
-
-
-    /* final SortableCarTableView carTableView = (SortableCarTableView) findViewById(R.id.tableView);
-        if (carTableView != null) {
-            final CarTableDataAdapter carTableDataAdapter = new CarTableDataAdapter(this, DataFactory.createCarList(), carTableView);
-            carTableView.setDataAdapter(carTableDataAdapter);
-            carTableView.addDataClickListener(new CarClickListener());
-            carTableView.addDataLongClickListener(new CarLongClickListener());
-            carTableView.setSwipeToRefreshEnabled(true);
-            carTableView.setSwipeToRefreshListener(new SwipeToRefreshListener() {
-                @Override
-                public void onRefresh(final RefreshIndicator refreshIndicator) {
-                    carTableView.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            final Car randomCar = getRandomCar();
-                            carTableDataAdapter.getData().add(randomCar);
-                            carTableDataAdapter.notifyDataSetChanged();
-                            refreshIndicator.hide();
-                            Toast.makeText(MainActivity.this, "Added: " + randomCar, Toast.LENGTH_SHORT).show();
-                        }
-                    }, 3000);
-                }
-            });*/
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/ntpro/mobileandroiddevtestwork/MainViewModel.kt
+++ app/src/main/java/com/ntpro/mobileandroiddevtestwork/MainViewModel.kt
@@ -3,3 +3,16 @@ package com.ntpro.mobileandroiddevtestwork
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+class MainViewModel: ViewModel() {
+    private val dealLiveData = MutableLiveData<MutableList<Server.Deal>?>()
+
+    init {
+        dealLiveData.value = ArrayList()
+    }
+
+    fun addDealPacket(deal: Server.Deal) {
+        dealLiveData.value?.add(deal)
+        dealLiveData.value = dealLiveData.value
+    }
+
+}
